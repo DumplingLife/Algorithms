6bf7ed8ba750a970021981f7cca7e6e65553cf03
Jason <dumplinglife7584@gmail.com>
2021-09-28 12:43:35 -0700

    test from school

--- src/test/TestString.java
+++ src/test/TestString.java
@@ -3,6 +3,7 @@ package test;
 public class TestString 
 {
 	public static void main(String[] args) {
+		//qwer
 		String s = "ieaiogerhgijnbqaklerhguioqerughoeruiqvmimqurgierwvmouuyhrshfjkghruiothoiwuvmuithguirghwuo";
 		SuffixArrayPrefixHasher sa = new SuffixArrayPrefixHasher(s);
 		for(int i = 0;i<1000000;i++) {
